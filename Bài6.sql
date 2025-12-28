386e7a1a0bc527d68d09cfee47e4e9da028b00c6
Megood1210 <xuanvinhle06@gmail.com>
2025-12-28 21:09:02 +0700

    Upload bài tập 6

---
+++ Bài6.sql
@@ -0,0 +1,15 @@
+create database session2;
+Use session2;
+
+create table result(
+	student_id int not null unique,
+    subject_id int not null unique,
+    progress_mark decimal not null CHECK (progress_mark >= 0 && progress_mark <= 10),
+    primary key(student_id, subject_id),
+    foreign key(student_id) references
+    student(student_id),
+    foreign key(subject_id) references
+    subject(subject_id)
+);
+
+
